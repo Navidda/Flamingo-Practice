ee544becc732bf864018da399ccab795b7e74861
Navid RahimiDanesh <naviddanesh76@gmail.com>
2016-08-09 16:44:11 +0430

    index + css added

--- manage.py
+++ manage.py
@@ -1,15 +1,24 @@
 import sys, os
 from shutil import copytree
 
+def init_index(data): # TODO
+    pass
+
+def init_about(data): # TODO
+    pass
+
 def create_weblog():
     id = raw_input('Weblog ID: ')
     name = raw_input('Weblog Name: ')
     welcome = raw_input('Welcome Text: ')
     email = raw_input('Email: ')
     tele = raw_input('Telegram ID: ')
-    copytree(os.path.dirname(__file__)+'/base_template', 'templates')
+    data = {'id': id, 'name': name, 'welcome': welcome, 'email': email, 'tele': tele}
     os.mkdir('posts')
     os.mkdir('result')
+    copytree(os.path.dirname(__file__)+'/base_template', 'templates')
+    init_index(data)
+    init_about(data)
 
 def main(args):
     if len(args) == 2 and args[0] == 'create' and args[1] == 'weblog':
